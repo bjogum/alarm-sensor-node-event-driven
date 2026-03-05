e3119a5127014b16d5a754bd301eabbaf4c7dabe
bjogum <bjogum@gmail.com>
2026-03-05 12:48:16 +0100

    upd comminucation files

--- src/wifi_manager.cpp
+++ src/wifi_manager.cpp
@@ -1,7 +1,14 @@
 #include <WiFi3.h>
 
 void maintainWiFi(){
+    if (WiFi.status() == WL_CONNECTED){
+        return;
+    }
+
+    
+
+
     // kolla om vi är anslutan till wifi
-    // om inte, försök ansluta
-    // sätt 'wifiConnected' variabel till = false
+    // om inte - kolla så vi fått tid sen senast -> isf: försök ansluta igen.
+    // sätt 'wifiConnected' variabel till = false .. bool func() return
 }
